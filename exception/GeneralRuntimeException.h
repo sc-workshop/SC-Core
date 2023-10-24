8a84d26de4750ce76e69178f95a993af3764b398
Daniil-SV <daniilsivacenko@gmail.com>
2023-10-24 23:39:52 +0300

    minor file update

--- exception/GeneralRuntimeException.h
+++ exception/GeneralRuntimeException.h
@@ -8,32 +8,10 @@ namespace sc
 {
 	class GeneralRuntimeException : std::exception
 	{
-	private:
-		char* m_file = nullptr;
-		int m_line = -1;
-
 	public:
-		GeneralRuntimeException() {};
-		GeneralRuntimeException(char* file, int line) : m_file(file), m_line(line)
-		{}
-
-		virtual const char* what() const
+		virtual const char* what() const override
 		{
 			return "Unknown runtime error";
-		}
-
-		virtual char* label() const
-		{
-			return "Unknown runtime exception";
-		}
-
-		std::string virtual format()
-		{
-			std::stringstream message;
-			message << label() << std::endl;
-			message << "Filename \"" << m_file << "\", line " << m_line << std::endl;
-
-			return message.str();
-		}
+		};
 	};
 }
