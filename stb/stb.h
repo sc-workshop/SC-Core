dc9bb30c30af1548d45ee3102f363812e8641b80
Daniil-SV <daniilsivacenko@gmail.com>
2023-10-29 20:43:03 +0300

    stb headers update

--- stb/stb.h
+++ stb/stb.h
@@ -1,7 +1,18 @@
+#pragma once
 
 #include "memory/alloc.h"
 
 #define STBIR_MALLOC(size, c) ((void)(c), sc::memalloc(size))
 #define STBIR_FREE(ptr,c)    ((void)(c), free(ptr))
 
+#define STBI_MALLOC(sz)           sc::memalloc(sz)
+#define STBI_REALLOC(p,newsz)     realloc(p,newsz)
+#define STBI_FREE(p)              free(p)
+
+#define STBIW_MALLOC(sz)        sc::memalloc(sz)
+#define STBIW_REALLOC(p,newsz)  realloc(p,newsz)
+#define STBIW_FREE(p)           free(p)
+
+#include "stb_image.h"
+#include "stb_image_write.h"
 #include "stb_image_resize.h"
