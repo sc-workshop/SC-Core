1d7803c032e5a84f8949738f80a3bbe46c0cf6bb
Daniil-SV <daniilsivacenko@gmail.com>
2023-10-29 15:41:52 +0300

    declarations mini-fix for CompressedImage

--- generic/image.h
+++ generic/image.h
@@ -84,8 +84,8 @@ namespace sc
     class CompressedImage : public Image
     {
     public:
-        virtual size_t decompressed_data_length();
-        virtual void decompress_data(Stream& buffer);
+        virtual size_t decompressed_data_length() = 0;
+        virtual void decompress_data(Stream& buffer) = 0;
     };
 
     class RawImage : public Image
