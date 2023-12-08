a58dff18107d6edbf4d521df4b1d7490e205e9ff
Daniil-SV <daniilsivacenko@gmail.com>
2023-12-08 19:11:08 +0200

    Fixed copy function for Raw Image

--- generic/image/raw_image.cpp
+++ generic/image/raw_image.cpp
@@ -95,58 +95,71 @@ namespace sc
 	/// <param name="image"></param>
 	void RawImage::copy(RawImage& image) const
 	{
-		size_t data_size = Image::calculate_image_length(m_width, m_height, m_depth);
+		size_t data_size = Image::calculate_image_length(image.width(), image.height(), image.depth());
 		uint8_t* data = nullptr;
-		Image::PixelDepth depth = m_depth;
+
+		Image::PixelDepth base_depth = m_depth;
+		switch (m_depth)
+		{
+			// RGBA
+		case sc::Image::PixelDepth::RGBA4:
+		case sc::Image::PixelDepth::RGB5_A1:
+			base_depth = Image::PixelDepth::RGBA8;
+			break;
+
+			// RGB
+		case sc::Image::PixelDepth::RGB565:
+			base_depth = Image::PixelDepth::RGB8;
+			break;
+
+		case sc::Image::PixelDepth::RGB8:
+		case sc::Image::PixelDepth::LUMINANCE8_ALPHA8:
+		case sc::Image::PixelDepth::LUMINANCE8:
+		case sc::Image::PixelDepth::RGBA8:
+		default:
+			break;
+		}
 
 		if (image.width() != m_width || image.height() != m_height)
 		{
-			uint8_t* resize_data = nullptr;
+			uint8_t* base_depth_data = nullptr;
 
-			switch (m_depth)
+			if (base_depth != m_depth)
 			{
-				// RGBA
-			case sc::Image::PixelDepth::RGBA4:
-			case sc::Image::PixelDepth::RGB5_A1:
-				depth = Image::PixelDepth::RGBA8;
-				break;
-
-				// RGB
-			case sc::Image::PixelDepth::RGB565:
-				depth = Image::PixelDepth::RGB8;
-				break;
-
-			case sc::Image::PixelDepth::RGB8:
-			case sc::Image::PixelDepth::LUMINANCE8_ALPHA8:
-			case sc::Image::PixelDepth::LUMINANCE8:
-			case sc::Image::PixelDepth::RGBA8:
-			default:
-				break;
+				base_depth_data = memalloc(Image::calculate_image_length(m_width, m_height, base_depth));
+				Image::remap(m_data, base_depth_data, m_width, m_height, m_depth, base_depth);
 			}
 
-			if (depth != m_depth)
-			{
-				resize_data = memalloc(Image::calculate_image_length(m_width, m_height, depth));
-				Image::remap(m_data, resize_data, m_width, m_height, m_depth, depth);
-			}
-
-			data = memalloc(Image::calculate_image_length(image.width(), image.height(), depth));
+			data = memalloc(Image::calculate_image_length(image.width(), image.height(), m_depth));
 			Image::resize(
-				resize_data ? resize_data : m_data, data,
+				base_depth_data ? base_depth_data : m_data, data,
 				m_width, m_height,
 				image.width(), image.height(),
 				m_type, m_space
 			);
 
-			if (resize_data)
+			if (base_depth_data)
 			{
-				free(resize_data);
+				free(base_depth_data);
 			}
 		}
 
 		if (image.depth() != m_depth)
 		{
-			Image::remap(data ? data : m_data, image.data(), image.width(), image.height(), depth, image.depth());
+			Image::remap(
+				data ? data : m_data, image.data(),
+				image.width(), image.height(),
+				base_depth, image.depth()
+			);
+		}
+		else
+		{
+			memcopy(data ? data : m_data, image.data(), data_size);
+		}
+
+		if (data)
+		{
+			free(data);
 		}
 	};
 
