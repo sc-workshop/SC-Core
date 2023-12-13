dc370702941aaea4ae4e72e8bc486fdbcd4587cb
Daniil-SV <daniilsivacenko@gmail.com>
2023-12-14 00:00:10 +0200

    minor changes

--- cmake/constants.cmake
+++ cmake/constants.cmake
@@ -1,6 +1,5 @@
-
-macro(set_global var_name var_value )
-    set(${var_name} ${var_value}  CACHE INTERNAL "")
+macro(set_global var_name var_value)
+    set(${var_name} ${var_value} CACHE INTERNAL "")
 endmacro()
 
 set_global(SC_GNU_fe "$<STREQUAL:${CMAKE_CXX_COMPILER_FRONTEND_VARIANT},GNU>")
@@ -10,7 +9,7 @@ set_global(SC_MSVC_fe "$<STREQUAL:${CMAKE_CXX_COMPILER_FRONTEND_VARIANT},MSVC>")
 set_global(SC_GNU "$<OR:${SC_GNU_fe},${SC_GNU_APPLE_fe}>")
 set_global(SC_MSVC "$<AND:${SC_MSVC_fe},$<CXX_COMPILER_ID:MSVC>>")
 set_global(SC_DEBUG "$<CONFIG:Debug>")
-set_global(SC_RELEASE "$<OR:$<CONFIG:Release>,$<CONFIG:RelWithDebInfo>,$<CONFIG:MinSizeRel>")
+set_global(SC_RELEASE "$<CONFIG:Release,RelWithDebInfo,MinSizeRel>")
 
 set_global(SC_X86_64 "$<STREQUAL:${CMAKE_SYSTEM_PROCESSOR},x86_64>")
 set_global(SC_AARCH64 "$<STREQUAL:${CMAKE_SYSTEM_PROCESSOR},aarch64>")
