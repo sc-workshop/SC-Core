6fda1924bc0ca5f2992190b12ea3b33da4be415d
pavidloq <sokov302@gmail.com>
2024-06-10 11:30:53 +0300

    added argparse

---
+++ supercell-core/source/core/console/console.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "core/3rdparty/argparse/argparse.h"
+
+namespace sc
+{
+	using namespace argparse;
+
+	// TODO(pavidloq): should I do this?
+	/*
+	struct Console
+	{
+		
+		enum class Color : std::uint8_t
+		{
+			Black,
+			Blue,
+			Cyan,
+			DarkBlue,
+			DarkCyan,
+			DarkGray,
+			DarkGreen,
+			DarkMagenta,
+			DarkRed,
+			DarkYellow,
+			Gray,
+			Green,
+			Magenta,
+			Red,
+			White,
+			Yellow
+		};
+
+		static void set_foregroud_color(Color color);
+		static void set_background_color(Color color);
+
+		static void clear_foreground_color();
+		static void clear_background_color();
+		
+	};
+	*/
+}
